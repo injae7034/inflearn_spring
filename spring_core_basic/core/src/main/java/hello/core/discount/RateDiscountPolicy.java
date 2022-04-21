f6109507e7c2372569e367d4912b8f55afeed58b
injae7034 <injae7034@gmail.com>
2022-04-21 18:14:26 +0900

    의존관계 자동 주입

--- spring_core_basic/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ spring_core_basic/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy")
+@MainDiscountPolicy//직접 만든 어노테이션(오타로 인한 컴파일 오류 잡아줌)
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
