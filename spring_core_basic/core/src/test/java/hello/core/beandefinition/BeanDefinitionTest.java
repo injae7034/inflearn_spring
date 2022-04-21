df9c4f9385e59ff0c8cc756772235fada20dea8c
injae7034 <injae7034@gmail.com>
2022-04-21 10:55:46 +0900

    스프링 컨테이너와 스프링 빈 예제 코드 작성 및 실습

---
+++ spring_core_basic/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,45 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName
+                        + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+    @Test
+    void xmlAppContext() {
+        GenericXmlApplicationContext ac2 = new GenericXmlApplicationContext("appConfig.xml");
+        String[] beanDefinitionNames = ac2.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac2.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName
+                        + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
