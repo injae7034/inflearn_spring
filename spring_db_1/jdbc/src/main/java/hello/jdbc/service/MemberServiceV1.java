5ef2211ff2cb106e86bc30dcac9958d9f7f89462
injae7034 <injae7034@gmail.com>
2022-07-10 15:43:56 +0900

    트랜잭션 처리 적용

--- spring_db_1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ spring_db_1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -12,6 +12,7 @@ public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        //트랜잭션 시작
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
@@ -20,7 +21,7 @@ public class MemberServiceV1 {
         validation(toMember);
 
         memberRepository.update(toId, fromMember.getMoney() + money);
-
+        //커밋 또는 롤백
     }
 
     private void validation(Member toMember) {
