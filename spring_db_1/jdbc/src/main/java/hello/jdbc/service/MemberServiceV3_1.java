75de467fe06e9c58fb9689bda568dad990e424ce
injae7034 <injae7034@gmail.com>
2022-07-11 12:25:04 +0900

    안쓰는 메소드 제거

--- spring_db_1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ spring_db_1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -54,15 +54,4 @@ public class MemberServiceV3_1 {
         }
     }
 
-    private void release(Connection con) {
-        if (con != null) {
-            try {
-                con.setAutoCommit(true); // 커넥션 풀에 반환하기 전에 default값인 true로 다시 초기화한 뒤
-                con.close(); // 다시 커넥션 풀에 반환함.
-            } catch (Exception e) {
-                log.info("error", e);
-            }
-        }
-    }
-
 }
