8ca881b4118d05637bbc456fab5f8e78f40f8e06
injae7034 <injae7034@gmail.com>
2022-07-28 15:22:45 +0900

    사용자 등록을 위한 API 구현 - POST HTTP Method

--- spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -2,6 +2,8 @@ package com.example.restfulwebservice.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +24,11 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
     }
 
 }
