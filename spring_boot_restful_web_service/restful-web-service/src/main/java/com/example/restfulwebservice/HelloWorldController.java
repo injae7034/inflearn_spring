cd73262a2d59ce3dd58d5df5d2327a2373d4ff86
injae7034 <injae7034@gmail.com>
2022-07-28 14:23:03 +0900

    PathVariable 사용

--- spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
