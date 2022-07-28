694b59cf8df93db19d2ff0f7e227473e68b062a6
injae7034 <injae7034@gmail.com>
2022-07-28 14:09:24 +0900

    json형태로 출력

--- spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -15,4 +15,9 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
 }
