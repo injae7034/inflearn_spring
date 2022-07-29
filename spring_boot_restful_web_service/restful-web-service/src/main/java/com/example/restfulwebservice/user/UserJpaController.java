25db1ee5d3535de24bc7b477ab578973d3377ba0
injae7034 <injae7034@gmail.com>
2022-07-29 15:50:57 +0900

    JPA와 연동하여 사용자 이름 변경 기능 추가

--- spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -1,21 +1,26 @@
 package com.example.restfulwebservice.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -23,21 +28,22 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
+@RequiredArgsConstructor
 public class UserJpaController {
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserJpaService userJpaService;
 
     @Autowired
     private PostRepository postRepository;
 
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
-        return userRepository.findAll();
+        return userJpaService.findAllUsers();
     }
 
     @GetMapping("/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id) {
-        Optional<User> user = userRepository.findById(id);
+        Optional<User> user = userJpaService.findUser(id);
 
         if (!user.isPresent()) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
@@ -54,12 +60,12 @@ public class UserJpaController {
 
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        userRepository.deleteById(id);
+        userJpaService.deleteUser(id);
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User savedUser = userRepository.save(user);
+        User savedUser = userJpaService.addUser(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
@@ -69,9 +75,26 @@ public class UserJpaController {
         return ResponseEntity.created(location).build();
     }
 
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id,
+                                           @RequestBody Map<String, String> userName) {
+        Optional<User> user = userJpaService.findUser(id);
+
+        if (!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        userJpaService.updateUser(user.get(), userName.get("name"));
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .build().toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
     @GetMapping("/users/{id}/posts")
     public List<Post> retrieveAllPostsByUser(@PathVariable int id) {
-        Optional<User> user = userRepository.findById(id);
+        Optional<User> user = userJpaService.findUser(id);
 
         if (!user.isPresent()) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
@@ -82,7 +105,7 @@ public class UserJpaController {
 
     @PostMapping("/users/{id}/posts")
     public ResponseEntity<Post> createPost(@PathVariable int id, @RequestBody Post post) {
-        Optional<User> user = userRepository.findById(id);
+        Optional<User> user = userJpaService.findUser(id);
 
         if (!user.isPresent()) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
---
+++ spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaService.java
@@ -0,0 +1,39 @@
+package com.example.restfulwebservice.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserJpaService {
+
+    private final UserRepository repository;
+
+    public List<User> findAllUsers() {
+        return repository.findAll();
+    }
+
+    public Optional<User> findUser(int id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public void deleteUser(int id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public User addUser(User user) {
+        return repository.save(user);
+    }
+
+    @Transactional
+    public void updateUser(User user, String name) {
+        user.setName(name);
+    }
+
+}
