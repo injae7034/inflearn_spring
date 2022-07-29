72d69f7a4706f77617f7d64d4b2c637955c082d4
injae7034 <injae7034@gmail.com>
2022-07-29 15:00:32 +0900

    사용자 추가와 삭제 기능 구현

--- spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -3,11 +3,18 @@ package com.example.restfulwebservice.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,7 +32,7 @@ public class UserJpaController {
         return userRepository.findAll();
     }
 
-    @GetMapping("users/{id}")
+    @GetMapping("/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id) {
         Optional<User> user = userRepository.findById(id);
 
@@ -40,5 +47,22 @@ public class UserJpaController {
         model.add(linkTo.withRel("all-users"));
 
         return model;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
