615c0d91b53549775efbeee69328489dabf3485a
injae7034 <injae7034@gmail.com>
2022-07-30 14:11:33 +0900

    Update CustomizedResponseEntityExceptionHandler.java

--- spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ spring_boot_restful_web_service/restful-web-service/src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -6,16 +6,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
-@RestController
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
