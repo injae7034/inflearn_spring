6c729d6f90296e0e5fd685120c77f244cbb40ba9
injae7034 <injae7034@gmail.com>
2022-08-09 16:21:58 +0900

    login 기능 구현

--- spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user-service/")
+@RequestMapping("/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
     @Autowired
--- spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,20 +1,45 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity  {
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
+    public static final String MY_IP_ADDRESS = "192.168.4.4";
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = getAuthenticationFilter(http);
 
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress(MY_IP_ADDRESS)
+                .and()
+                .authenticationManager(authenticationManager)
+                .addFilter(getAuthenticationFilter(authenticationManager));
+
+//        http.authorizeRequests()
+//                .antMatchers("/actuator/**").permitAll() // actuator permitAll
+//                .antMatchers("/error/**").permitAll()
+//                .antMatchers("/**").hasIpAddress(MY_IP_ADDRESS)
+//                .and()
+//                .authenticationManager(authenticationManager)
+//                .addFilter(getAuthenticationFilter(authenticationManager));
 
         // frame load 하게 해줌
         http.headers().frameOptions().disable();
@@ -22,4 +47,13 @@ public class WebSecurity  {
         return http.build();
     }
 
+    private AuthenticationManager getAuthenticationFilter(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new AuthenticationFilter(authenticationManager, userService, env);
+    }
 }
