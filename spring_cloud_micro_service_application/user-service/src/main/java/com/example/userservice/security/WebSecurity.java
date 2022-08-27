68cd0e36a2e3cd67e92ab6d8791b955a7a753202
injae7034 <injae7034@gmail.com>
2022-08-27 14:17:45 +0900

    Update WebSecurity.java

--- spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -19,7 +19,7 @@ public class WebSecurity  {
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final Environment env;
-    public static final String MY_IP_ADDRESS = "192.168.4.5";
+    public static final String MY_IP_ADDRESS = "192.168.4.2";
 
 
     @Bean
