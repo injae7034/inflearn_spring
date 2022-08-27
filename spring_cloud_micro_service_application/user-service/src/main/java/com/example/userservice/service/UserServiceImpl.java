6f2489030579cab085adf5870afbd630cd5b5854
injae7034 <injae7034@gmail.com>
2022-08-28 00:07:17 +0900

    Update UserServiceImpl.java

--- spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ spring_cloud_micro_service_application/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -90,10 +90,12 @@ public class UserServiceImpl implements UserService {
 //        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
 
         //circuitBreaker 사용
+        log.info("Before call orders microservice");
         CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitBreaker");
         List<ResponseOrder> orderList = circuitBreaker.run(() -> orderServiceClient.getOrders(userId),
                 throwable -> new ArrayList<>());
-        
+        log.info("After called orders microservice");
+
         userDto.setOrders(orderList);
 
         return userDto;
